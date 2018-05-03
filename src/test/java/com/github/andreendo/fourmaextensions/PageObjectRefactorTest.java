073e200d0a42e0b878bd615f00a4e91ea437a6c5
andreendo <andreendo@gmail.com>
2018-05-03 16:17:38 -0300

    first commit

---
+++ src/test/java/com/github/andreendo/fourmaextensions/PageObjectRefactorTest.java
@@ -0,0 +1,19 @@
+package com.github.andreendo.fourmaextensions;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author andreendo
+ */
+public class PageObjectRefactorTest {
+    
+    public PageObjectRefactorTest() {
+    }
+
+    @Test
+    public void test01() {
+    
+    }
+}
